739f2381314438f7f439f0b9af0fca05ae70e342
Stefan Fluit <stefan@fluit-online.nl>
2021-03-24 10:33:50 +0100

    Removed rotated API key used for testing

--- src/config.sh
+++ src/config.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-export GITLAB_API_KEY="1ot4ZTqqn7EmzG-Dc23R"
-export SSH_USER="frank"
-export TMP_DIR="/tmp/pnd-server"
+# Change before running
+export GITLAB_API_KEY=""
+export SSH_USER=""
 
 # SSH
 # Provide a path to a pub key, e.g., /home/fluit/.ssh/id_rsa.pub or leave empty to create a new one.
 export SSH_KEY="/home/fluit/.ssh/id_rsa.pub"
 export SSH_KEY_OUTPUT=$(<${SSH_KEY})
 export SSH_ID_RSA=$(echo "${SSH_KEY}" | cut -f1,2 -d'.')
+
+# No need to change
+export TMP_DIR="/tmp/pnd-server"
