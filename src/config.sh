0ed0c7c03a31f5b13cdfcf0cc229f4c5482fb340
Stefan Fluit <stefan@fluit-online.nl>
2021-03-28 22:00:33 +0200

    Added Git repo variable

--- src/config.sh
+++ src/config.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# Change before running
+# Git variables
 export GITLAB_API_KEY=""
+export DEPLOY_REPO="git@gitlab.com:Santralos/pnd-binance.git"
 
 # Sets SSH user to your name, or keep it like this. In this situation it will use your TTY user.
 export SSH_USER="$(whoami)"
