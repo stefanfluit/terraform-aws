7def134c6e4515d2a9ee98e2aeb1887f5aa87d5d
Stefan Fluit <stefan@fluit-online.nl>
2021-04-01 20:11:48 +0200

    Added comments

--- src/config.sh
+++ src/config.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
 # Git variables
-export GITLAB_API_KEY=""
-export DEPLOY_REPO="git@gitlab.com:Santralos/pnd-binance.git"
-export BASENAME_REPO=$(basename "${DEPLOY_REPO}" .git)
-export GIT_DEPTH="10"
-export SSH_USER="$(whoami)"
+export GITLAB_API_KEY=""                                                                                # Gitlab API key.
+export DEPLOY_REPO="git@gitlab.com:Santralos/pnd-binance.git"                                           # The Git repo URL.
+export BASENAME_REPO=$(basename "${DEPLOY_REPO}" .git)                                                  # Basename of the repo. 
+export GIT_DEPTH="10"                                                                                   # The depth of the git clone to not fetch the whole repo, it's to big and will give errors. 
+export SSH_USER="$(whoami)"                                                                             # This can be editted if you run this as a different user. 
 
 # Alerting, only set what you want to use.
-export DISCORD_WEBHOOK=""
-export SLACK_WEBHOOK=""
-export SLACK_CHANNEL=""
+export DISCORD_WEBHOOK=""                                                                               # If this is set, the script will notify using Discord.
+export SLACK_WEBHOOK=""                                                                                 # If this is set, the script will notify using Slack.
+export SLACK_CHANNEL=""                                                                                 # Set this if the channel differantiate from the Slack default channel for the API key. 
 
 # AWS
-export AWS_REGION="ap-northeast-3"
-export AWS_PROFILE="default"
-export AWS_INSTANCE="t3.nano"
+export AWS_REGION="ap-northeast-3"                                                                      # Change region here, it will use sed to replace it in the file of Terraform. Overriden by ~/.aws/config.
+export AWS_PROFILE="default"                                                                            # Usually your AWS profile is default. 
+export AWS_INSTANCE="t3.nano"                                                                           # The AWS instance, change here but don't go lower as it is not compatible with the projects region.
 
 # SSH Key
-export SSH_KEY="/home/${SSH_USER}/.ssh/id_rsa.pub"
-export SSH_KEY_OUTPUT=$(<${SSH_KEY})
-export SSH_ID_RSA=$(echo "${SSH_KEY}" | cut -f1,2 -d'.')
+export SSH_KEY="/home/${SSH_USER}/.ssh/id_rsa.pub"                                                      # The path to pubkey, here to be changed if it does not equal to your username.
+export SSH_KEY_OUTPUT=$(<${SSH_KEY})                                                                    # The pubkey without useless cat.
+export SSH_ID_RSA=$(echo "${SSH_KEY}" | cut -f1,2 -d'.')                                                # Get the name of the id_rsa without the .pub part
 
 # No need to change
-export TMP_DIR="/tmp/pnd-server"
-export VERSION_URL="https://raw.githubusercontent.com/stefanfluit/terraform-aws/master/VERSION"
-export LOG_LOG="/home/${SSH_USER}/.pnd-binance-builder.log"
+export TMP_DIR="/tmp/pnd-server"                                                                        # Just a tmp dir to put some files.
+export VERSION_URL="https://raw.githubusercontent.com/stefanfluit/terraform-aws/master/VERSION"         # A version file in the repo to check if you're on the latest.
+export LOG_LOG="/home/${SSH_USER}/.pnd-binance-builder.log"                                             # The log location for this script.
 
 # MongoDB
-export MONGO_HOST=""                                # The Mongo host.
-export MONGO_SSH_USER=""                            # The user you use to log in to the Mongo host.
+export MONGO_HOST=""                                                                                    # The Mongo host.
+export MONGO_SSH_USER=""                                                                                # The user you use to log in to the Mongo host.
