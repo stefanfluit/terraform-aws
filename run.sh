2a87168490323681fc8b24a8ff330501031152c1
Stefan Fluit <stefan@fluit-online.nl>
2021-04-03 20:08:59 +0200

    Add --ssh-test statement

--- run.sh
+++ run.sh
@@ -56,6 +56,10 @@ case "${args_}" in
             run_test
             exit
             ;;
+        --ssh-test)
+            vagrant_ssh
+            exit
+            ;;
 
         *)
             cli_log "No parameter specified."
