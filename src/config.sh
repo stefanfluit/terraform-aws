637b59068a08e9fda27c689d54a8f7cea60faa9a
Stefan Fluit <stefan@fluit-online.nl>
2021-04-04 15:25:37 +0200

    Add option to skip update if broken

--- src/config.sh
+++ src/config.sh
@@ -40,3 +40,4 @@ export VAGRANT_EXPERIMENTAL="cloud_init,disks"                                  
 
 # Other variables
 export WAN_IP=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')       # Attempt to get your IP for the Vagrant file > gitlab
+export SKIP_UPDATE="disabled"                                                                           # set to 'enabled' to skip update.
