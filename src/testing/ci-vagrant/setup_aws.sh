2d0aca24d373b859ef5690f6cf065ebd2508dfc0
Stefan Fluit <stefan@fluit-online.nl>
2021-04-04 17:52:08 +0200

    Add variables

--- src/testing/ci-vagrant/setup_aws.sh
+++ src/testing/ci-vagrant/setup_aws.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+declare AWS_DIR
+AWS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+. "${AWS_DIR}/../../build-config.sh"
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
 unzip /tmp/awscliv2.zip
-cd /tmp && sudo ./aws/install
+sudo ./tmp/aws/install
 printf "%s\n" "aws_access_key_id=${AWS_ACCES}" >> /home/vagrant/.aws/credentials
 printf "%s\n" "aws_secret_access_key=${AWS_SECRET}" >> /home/vagrant/.aws/credentials
 printf "%s\n" "region=${AWS_REGION}" >> /home/vagrant/.aws/config
