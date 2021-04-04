6fd4ab2a955ad851f0e66b00c63176b0326a74f4
Stefan Fluit <stefan@fluit-online.nl>
2021-04-04 18:08:17 +0200

    Fix, hopefully

--- src/testing/ci-vagrant/setup_aws.sh
+++ src/testing/ci-vagrant/setup_aws.sh
@@ -6,10 +6,13 @@ AWS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 . "${AWS_DIR}/../../build-config.sh"
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
-unzip /tmp/awscliv2.zip
+unzip /tmp/awscliv2.zip &> /dev/null
 sudo ./tmp/aws/install
+
+mkdir /home/vagrant/.aws
 printf "[default]\n" > /home/vagrant/.aws/credentials
+printf "[default]\n" > /home/vagrant/.aws/config
+
 printf "%s\n" "aws_access_key_id=${AWS_ACCES}" >> /home/vagrant/.aws/credentials
 printf "%s\n" "aws_secret_access_key=${AWS_SECRET}" >> /home/vagrant/.aws/credentials
-printf "[default]\n" > /home/vagrant/.aws/config
 printf "%s\n" "region=${AWS_REGION}" > /home/vagrant/.aws/config
