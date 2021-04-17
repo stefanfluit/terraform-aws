64c1cf3f6ea92dd491dbd861339abc8600679a28
Stefan Fluit <stefan@fluit-online.nl>
2021-04-17 13:14:21 +0200

    Supress install output for Terraform

--- src/functions.sh
+++ src/functions.sh
@@ -85,7 +85,7 @@ stamp_logfile() {
 install_terraform() {
     curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
     sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-    sudo apt-get update && sudo apt-get install terraform -y
+    sudo apt-get update && sudo apt-get install terraform -y &> /dev/null
 }
 
 check_installed() {
@@ -307,7 +307,11 @@ validate_config() {
 }
 
 test_config() {
-  echo "${GITLAB_API_KEY}" &> /dev/null && cli_log "Config seems to be valid." || cli_log "Config is not valid!"
+    if [ -n "${GITLAB_API_KEY}" ]; then
+        cli_log "Config seems to be valid."
+    else
+        cli_log "Config is not valid!" && exit 1;
+    fi
 }
 
 run_init() {
